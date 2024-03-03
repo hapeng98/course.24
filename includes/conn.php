81c5edfd0f89a63702ed348851dedf1e3596c257
hapeng98 <156978507+hapeng98@users.noreply.github.com>
2024-03-03 17:10:20 +0900

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,7 +1,7 @@
 <?php
 $servername='localhost';
 $username='root';
-$password='smiler042^^';
+$password='';
 $dbname='course';
 $conn=mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
